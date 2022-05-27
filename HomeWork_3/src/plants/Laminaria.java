dd37a994d2188f6fab3ea67e2c4c2ec166e69912
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2022-05-27 11:09:37 +0300

    Create Laminaria.java

---
+++ HomeWork_3/src/plants/Laminaria.java
@@ -0,0 +1,23 @@
+package plants;
+
+import java.awt.*;
+
+public class Laminaria extends Immobile {
+
+    public Laminaria(int x, int y,int size){
+        super(x, y,size);
+    }
+    public Laminaria(){
+        super(50, 50,30);
+    }
+    public void drawCreature(Graphics g){
+
+        g.setColor(Color.green);
+        g.fillArc(super.x-size/20, y-size, size/10, size*4/5,0,360);
+        g.fillArc(x-size*3/20, y-size*13/15, size/10, size*2/3,0,360);
+        g.fillArc(x-size/20, y-size*13/15, size/10, size*2/3,0,360);
+        g.drawLine(x,y,x,y-size/5);
+        g.drawLine(x,y,x-size/10,y-size/5);
+        g.drawLine(x,y,x+size/10,y-size/5);
+    }
+}
