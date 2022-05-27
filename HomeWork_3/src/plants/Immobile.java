fd49e0afd16599cf408d57fd0649ad64303ca8b0
navesf <navesf@ac.sce.ac.il>
2022-05-27 15:39:46 +0300

    Update Immobile.java

--- HomeWork_3/src/plants/Immobile.java
+++ HomeWork_3/src/plants/Immobile.java
@@ -8,19 +8,17 @@ public abstract class Immobile implements SeaCreature {
     protected int x;
     protected int y;
     protected int size;
-    protected Color color;
-    protected String name;
-    public Immobile(){
-        x =0;
-        y =0;
-        this.size=10;
-        this.color = Color.green;
+
+    public Immobile() {
+        x = 0;
+        y = 0;
+        this.size = 10;
     }
-    public Immobile(int x, int y, int size){
-        this.x =x;
-        this.y =y;
-        this.size=size;
-        this.color = Color.green;
+
+    public Immobile(int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
     }
     public abstract void drawCreature(Graphics g);
 }
