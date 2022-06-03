4af2e9c1eaa4cf1bcb808e7ab3095e7cb6d35df1
navesf <navesf@ac.sce.ac.il>
2022-06-03 18:30:22 +0300

    Update DecoratorDialog.java

--- HomeWork_3/src/start/DecoratorDialog.java
+++ HomeWork_3/src/start/DecoratorDialog.java
@@ -17,14 +17,18 @@ public class DecoratorDialog extends JDialog implements ActionListener{
     private JColorChooser jColorChooser;
     private int animalChosen;
     private Color colorChosen;
+    private JButton changeButtons;
+    private AquaPanel aquaPanel;
 
 
-    DecoratorDialog(JFrame parent, String title, boolean modal, int amount_Animal,Object[][] data) {
+    DecoratorDialog(JFrame parent, String title, boolean modal, int amount_Animal, Object[][] data, AquaPanel a) {
         super(parent, title, modal);
-        setLocation(450, 150);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension dimension = toolkit.getScreenSize();
-        this.setBounds(dimension.width / 2 - 400, dimension.height / 2 - 400, 100, 100);
+        this.setBounds(dimension.width / 2 - 500, dimension.height / 2 - 400, 100, 100);
+        //setLocation(dimension.width/2, dimension.height/2);
+
+        aquaPanel = a;
 
         this.data = data;
         infoTable = new JTable(data, columnNames);
@@ -32,7 +36,10 @@ public class DecoratorDialog extends JDialog implements ActionListener{
         scrollPane.setVisible(true);
 
         JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(1, 2,20,20));
         p1.add(scrollPane);
+        jColorChooser = new JColorChooser();
+        p1.add(jColorChooser);
 
         JPanel p2 = new JPanel();
         JLabel jlabeltext = new JLabel("Select Animal to change color:");
@@ -41,17 +48,11 @@ public class DecoratorDialog extends JDialog implements ActionListener{
         for (int i = 1; i <= amount_Animal; i++)
             count[i-1] = String.valueOf(i);
         comboAmount = new JComboBox<>(count);
-        comboAmount.setPreferredSize(new Dimension(10, 20));
-
         p2.add(jlabeltext);
         p2.add(comboAmount);
-
-        JLabel banner = new JLabel("Welcome to the Tutorial Zone!",
-                JLabel.CENTER);
-        banner.setForeground(Color.yellow);
-        jColorChooser = new JColorChooser(banner.getForeground());
-        p2.add(jColorChooser, BorderLayout.PAGE_END);
-
+        changeButtons = new JButton("Change Color");
+        p2.add(changeButtons);
+        changeButtons.addActionListener(this);
 
         BorderLayout myBorderLayout1 = new BorderLayout();
         JPanel mainPanel = new JPanel();
@@ -73,7 +74,10 @@ public class DecoratorDialog extends JDialog implements ActionListener{
     }
 
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == changeButtons) {
+            colorChosen = jColorChooser.getColor();
+            aquaPanel.setColor(comboAmount.getSelectedIndex() + 1, colorChosen);
+        }
     }
 
 }
