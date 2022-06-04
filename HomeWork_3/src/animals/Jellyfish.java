af970fc7d8b72bf60ca6c667f9359bfff1f31975
tomerda3 <93253870+tomerda3@users.noreply.github.com>
2022-06-04 16:41:24 +0300

    Update Jellyfish.java

--- HomeWork_3/src/animals/Jellyfish.java
+++ HomeWork_3/src/animals/Jellyfish.java
@@ -18,6 +18,9 @@ public class Jellyfish extends Swimmable {
         super(30, 30, 30, 10, 10, 2, null,0);
     }
 
+    public Jellyfish(Jellyfish copy){
+        super(copy.size, copy.x_front, copy.y_front, copy.horSpeed, copy.verSpeed, copy.col, copy.ap,copy.eatingFreq);
+    }
     /**
      * A function that returns the class name "Jellyfish".
      * @return the name "Jellyfish" in String.
